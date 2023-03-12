267c3cf6b25eabe848f1b4ba17de1717958c1070
CatherineJOnia <catherinejonia@gmail.com>
2023-03-12 00:29:51 -0500

    removed unnecessary code

--- src/components/canvas/LogoShield.jsx
+++ src/components/canvas/LogoShield.jsx
@@ -4,11 +4,11 @@ import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 import CanvasLoader from "../Loader";
 
-const LogoShield = ({ name, icon, source_code_link }) => {
+const LogoShield = ({ icon }) => {
 
   return (
     <Suspense fallback={<CanvasLoader />}>
-      <Tilt className='xs:w-[100px] w-60'>
+      <Tilt className='xs:w-[100px]'>
         <motion.div
         whileHover={{ scale: 1.2 }}
           className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
@@ -26,9 +26,6 @@ const LogoShield = ({ name, icon, source_code_link }) => {
               alt='web-development'
               className='w-16 h-16 object-contain'
             />
-            <h3 className='text-white text-[20px] font-bold text-center'>
-              {name}
-            </h3>
           </div>
       </motion.div>
     </Tilt>
