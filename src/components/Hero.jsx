7a8f959f089956ab6be79222e586cc37f1606e53
CatherineJOnia <catherinejonia@gmail.com>
2023-03-12 21:18:45 -0400

    updated hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { MoonCanvas } from "./canvas";
+// import { MoonCanvas } from "./canvas";git
 
 import React from "react";
 
@@ -27,7 +27,7 @@ const Hero = () => {
         </div> */}
       </div>
 
-      <MoonCanvas />
+      {/* <MoonCanvas /> */}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
